c5ff980cba8e56071c20029a845775cabebd7561
MrGrindor <54775642+MrGrindor@users.noreply.github.com>
2020-07-29 18:53:10 +0200

    Added Empire Fleet and Ship Classes

---
+++ Classes/Fleet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StellarisSaveParser.Classes
+{
+    public class Fleet
+    {
+        public int FleetId { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public double MilitaryPower { get; set; }
+
+    }
+}
---
+++ Classes/Ships.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StellarisSaveParser.Classes
+{
+    public class Ships
+    {
+        public int ShipId { get; set; }
+        public int FleetId { get; set; }
+        public string ShipName { get; set; }
+        public string type { get; set; }
+    }
+}
